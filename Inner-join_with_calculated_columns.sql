ba07a37c3023d36a1d8111be38656884a9f2a4bd
Noel Van Koughnett <noel.vankoughnett@gmail.com>
2022-10-11 16:37:56 -0600

    testing upload

--- Inner-join_with_calculated_columns.sql
+++ Inner-join_with_calculated_columns.sql
@@ -7,15 +7,15 @@ SELECT
 	((MSRP-priceEach)/MSRP*100) AS percent
 	 /*Here I've created "calculated columns" for discount and percent
 	  *to show the business which products they should be focusing
-	  their sale on.*/
+	  their sales on.*/
 FROM 
 	products p 
 INNER JOIN orderdetails o
 	ON p.productCode = o.productCode
 		AND p.MSRP > o.priceEach
 WHERE p.productCode = 'S10_1678'
-/*Here we've created an inner join on the products and orderdetails tables and are searching for 
- *a specific productcode. We're also looking at items where MSRP is greater than price paid then
+/*Here I've created an inner join on the products and orderdetails tables and am searching for 
+ *a specific productcode. I'm also looking at items where MSRP is greater than price paid then
  *I also searched if any instances existed where priceEach was greater than MRSP and
  *fortunely, none existed as this would mean the company would not make any
  *profit on the sale of those itmes*/
@@ -23,4 +23,4 @@ GROUP BY
 	orderNumber
 ORDER BY 
 	percent DESC;
-/*Lastly, I group by orderNumber and order percent in decending order*/
+/*Lastly, I grouped by orderNumber and ordered percent in decending order*/
